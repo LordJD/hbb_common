72b70847cc09250de498ba4a20ebb96e471d6425
LordJD <103748140+LordJD@users.noreply.github.com>
2025-08-17 02:24:26 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["193.32.149.184"];
+pub const RS_PUB_KEY: &str = "kxUDKTU8PX5NfCoPXV6VT7dX3VqkpKl9i40jtSLkGLI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
